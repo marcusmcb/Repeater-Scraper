5c87f6415e8e1f3b01e2e226c27995334bf08e7d
Marcus McBride <marcusmcb@gmail.com>
2019-06-21 16:03:24 -0700

    added flask starter files

---
+++ app.py
@@ -0,0 +1,54 @@
+# python dependencies
+from bs4 import BeautifulSoup
+import pymongo
+from pymongo import MongoClient
+import requests
+from flask import Flask, render_template
+
+# create flask app
+app = Flask(__name__)
+
+# mongo connection
+conn = 'mongodb://localhost:27017'
+
+# connection to pymongo
+client = pymongo.MongoClient(conn)
+
+# database connection
+db = client.repeater_db
+
+# drops stations tables to avoid duplicates
+db.stations.drop()
+
+# url to scrape & soup setup
+url = 'https://www.repeaterbook.com/repeaters/Display_SS.php?state_id=06&band=4&loc=%&call=%&use=%'
+
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'html.parser')
+
+# scrapes url & pushes data to stations table in db
+table_rows = soup.find_all('tr')[4:]
+
+for item in table_rows:
+    freq = item.find('a').text
+    call_sign = item.find_all('td', attrs={'class': None})[3].text
+    county = item.find_all('td', attrs={'class': None})[2].text
+    location = item.find(class_="w3-left-align").text
+    usage = item.find('font').text
+    db.stations.insert_one(
+        {'location': location,
+         'frequency': freq,
+         'call sign': call_sign,
+         'county': county,
+         'usage': usage})
+
+# set route
+@app.route('/')
+def index():
+     stationlist = list(db.stations.find())
+     print(station_list)
+     return render_template('index.html', stationlist=stationlist)
+
+if __name__ == "__main__":
+     app.run(debug=True)
+
