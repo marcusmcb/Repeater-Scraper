e528ad8660b826e5f6fe0a5f4c7b7410612dfdd8
Marcus McBride <marcusmcb@gmail.com>
2019-06-26 10:50:06 -0700

    code cleanup, added pop-up element to each map point when clicked

--- static/js/app.js
+++ static/js/app.js
@@ -1,8 +1,13 @@
 function jsTest() {
+
+     // api endpoint to grab JSON data
      var url = "/api/repeaters";
+
+     // function to build transmitter map
      d3.json(url).then(function (response) {
           console.log(response[0].latitude);
 
+          // render map to html map div
           var myMap = L.map("map", {
                center: [34.0522, -118.2437],
                zoom: 8
@@ -15,8 +20,10 @@ function jsTest() {
                accessToken: API_KEY
           }).addTo(myMap);
 
+          // for loop to pull out coordinates and generate popup with added info when clicked
           for (var i = 0; i < response.length; i++) {
                L.marker([response[i].latitude, response[i].longitude])
+                    .bindPopup("<h4>" + response[i].call_sign + " / " + response[i].frequency + "</h4><hr><h6>" + response[i].location + " (" + response[i].usage + ")" + "</h6>")
                     .addTo(myMap)
           }
      })
