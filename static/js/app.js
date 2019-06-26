98a5805591566735c030bf31d72c8e339d236e60
Marcus McBride <marcusmcb@gmail.com>
2019-06-26 12:01:28 -0700

    added code to set marker color based on usage/status, added marker icons

--- static/js/app.js
+++ static/js/app.js
@@ -1,14 +1,42 @@
+// variables for marker colors
+let redIcon = new L.Icon({
+     iconUrl: 'static/img/marker-icon-2x-red.png',
+     shadowUrl: 'static/img/marker-shadow.png',
+     iconSize: [25, 41],
+     iconAnchor: [12, 41],
+     popupAnchor: [1, -34],
+     shadowSize: [41, 41]
+});
+
+let greenIcon = new L.Icon({
+     iconUrl: 'static/img/marker-icon-2x-green.png',
+     shadowUrl: 'static/img/marker-shadow.png',
+     iconSize: [25, 41],
+     iconAnchor: [12, 41],
+     popupAnchor: [1, -34],
+     shadowSize: [41, 41]
+});
+
+let blackIcon = new L.Icon({
+     iconUrl: 'static/img/marker-icon-2x-black.png',
+     shadowUrl: 'static/img/marker-shadow.png',
+     iconSize: [25, 41],
+     iconAnchor: [12, 41],
+     popupAnchor: [1, -34],
+     shadowSize: [41, 41]
+});
+
 function jsTest() {
 
      // api endpoint to grab JSON data
-     var url = "/api/repeaters";
+     let url = "/api/repeaters";
 
      // function to build transmitter map
      d3.json(url).then(function (response) {
           console.log(response[0].latitude);
 
           // render map to html map div
-          var myMap = L.map("map", {
+          let myMap = L.map("map", {
                center: [34.0522, -118.2437],
                zoom: 8
           });
@@ -21,10 +49,18 @@ function jsTest() {
           }).addTo(myMap);
 
           // for loop to pull out coordinates and generate popup with added info when clicked
-          for (var i = 0; i < response.length; i++) {
-               L.marker([response[i].latitude, response[i].longitude])
-                    .bindPopup("<h4>" + response[i].call_sign + " / " + response[i].frequency + "</h4><hr><h6>" + response[i].location + " (" + response[i].usage + ")" + "</h6>")
-                    .addTo(myMap)
+          for (let i = 0; i < response.length; i++) {
+
+               // if/else logic to determine marker color based on current usage status
+               if (response[i].usage === "OPEN") {
+                    L.marker([response[i].latitude, response[i].longitude], { icon: greenIcon })
+                         .bindPopup("<h4>" + response[i].call_sign + " / " + response[i].frequency + "</h4><hr><h6>" + response[i].location + " (" + response[i].usage + ")" + "</h6>")
+                         .addTo(myMap)
+               } else {
+                    L.marker([response[i].latitude, response[i].longitude], { icon: redIcon })
+                         .bindPopup("<h4>" + response[i].call_sign + " / " + response[i].frequency + "</h4><hr><h6>" + response[i].location + " (" + response[i].usage + ")" + "</h6>")
+                         .addTo(myMap)
+               }
           }
      })
 }
