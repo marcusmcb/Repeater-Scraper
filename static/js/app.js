cc326724893934ec719977df7c34775a925c6f72
Marcus McBride <marcusmcb@gmail.com>
2019-06-25 20:05:06 -0700

    added working leaflet map to app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,40 +1,25 @@
-// Create our initial map object
-// Set the longitude, latitude, and the starting zoom level
-var myMap = L.map("map", {
-     center: [34.0522, -118.2437],
-     zoom: 9
-});
-
-// Add a tile layer (the background map image) to our map
-// We use the addTo method to add objects to our map
-L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-     maxZoom: 18,
-     id: "mapbox.streets",
-     accessToken: API_KEY
-}).addTo(myMap);
-
-// Create a new marker
-// Pass in some initial options, and then add it to the map using the addTo method
-L.marker([45.52, -122.67], {
-     draggable: true,
-     title: "My First Marker"
-}).addTo(myMap);
-
-L.marker([45.42, -122.67], {
-     draggable: true,
-     title: "My First Marker"
-}).addTo(myMap);
-
-// Binding a pop-up to our marker
-//marker.bindPopup("Hello There!");
-
 function jsTest() {
      var url = "/api/repeaters";
      d3.json(url).then(function (response) {
-          console.log(response);
+          console.log(response[0].latitude);
+
+          var myMap = L.map("map", {
+               center: [34.0522, -118.2437],
+               zoom: 9
+          });
+
+          L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+               attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+               maxZoom: 18,
+               id: "mapbox.streets",
+               accessToken: API_KEY
+          }).addTo(myMap);
+
+          for (var i = 0; i < response.length; i++) {
+               L.marker([response[i].latitude, response[i].longitude])
+                    .addTo(myMap)
+          }
      })
 }
 
 jsTest();
-
